81f075cf22d6aee63bfa8eb420e0d8e375e3d016
ChristopherColton <50006085+ChristopherColton@users.noreply.github.com>
2020-11-16 16:13:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCoeU2pTdgRfa-8cglx1inLpRWjCrQMSCE",
+    authDomain: "lfgfinal-8a5b2.firebaseapp.com",
+    databaseURL: "https://lfgfinal-8a5b2.firebaseio.com",
+    projectId: "lfgfinal-8a5b2",
+    storageBucket: "lfgfinal-8a5b2.appspot.com",
+    messagingSenderId: "420537263604",
+    appId: "1:420537263604:web:93c86576a6d7fd12a345a1",
+    measurementId: "G-785TYXYP42"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
